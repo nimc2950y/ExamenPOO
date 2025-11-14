60664cbe66dc4b3ce9851f6a9a895a6a3cf4d963
nimc2950y <147666625+nimc2950y@users.noreply.github.com>
2025-11-14 16:26:36 -0500

    Examen completo con el ultimo ejercicio q me faltaba

--- main.scala
+++ main.scala
@@ -10,8 +10,8 @@ object main {
       }).sum                                     /* Suma todos los cuadrados */
       suma / Logs.size                           /* Devuelve la varianza del promedio de los cuadrados */
     }
-    
-    /* Segundo ejercicio  */                          
+
+    /* Segundo ejercicio  */
     val lista = List(100.0, 200.0, 350.0)              /*  lista de precios */
     val iva: Double => Double = p => p * 1.19          /* Política de la tienda : aplicar IVA del 19% */
     val descuento: Double => Double = p => p * 0.90       /* Política de la tienda : aplicar descuento del 10% */
@@ -19,14 +19,19 @@ object main {
                 politica_tienda: Double => Double): List[Double] = { /* y la política de la tienda para ajustar el precio */
       precios.map(politica_tienda)                                /* Aplica la política a cada precio y devuelve nueva lista */
     }
-    val conIva = precios(lista, iva)                   /* Aplica política IVA a la lista */
-    val conDescuento = precios(lista, descuento)          /* Aplica política descuento a la lista */
+    val ivap = precios(lista, iva)                   /* Aplica política del iva a la lista */
+    val descuentop = precios(lista, descuento)          /* Aplica política descuento a la lista */
     println(s"precio original: $lista")                /* Muestra el precio original */
-    println(s"iva: $conIva")                       /* Muestra el precio con iva */
-    println(s"descuento  $conDescuento")           /* Muestra el precio con el descuento del 10% */
-    
+    println(s"iva: $ivap")                       /* Muestra el precio con iva */
+    println(s"descuento  $descuentop")           /* Muestra el precio con el descuento del 10% */
+
     /* Tercer Ejercicio*/
+    def generadorIncrementador(nombre1: Int): Int => Int =     /* Funcion con nombre1 */
+      (nombre2: Int) => nombre2 + nombre1                      /* Funcion resultante: suma el incremento capturado al argumento que es nombre2 */
     
-  
   }
 }
+
+
+
+
